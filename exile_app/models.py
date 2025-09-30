42ccef58dde88ef39a236e122434344a71eb62c8
Маслакова Мария <masamaslakova@gmail.com>
2025-09-30 23:59:08 +0500

    first

---
+++ exile_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Users(models.Model):
+    name = models.CharField(verbose_name='Имя пользователя', max_length=20)
+    surname = models.CharField('Фамилия пользователя', max_length=25)
+    birthday = models.IntegerField('Номер телефона')
+    bio = models.TextField('Описание')
+
+class Meta:
+    verbose_name = "Автор"
+    verbose_name_plural = "Авторы"
+    ordering = ["surname", "name"]
+    indexes = [
+        models.Index(fields=["surname"])
+    ]
+    constraints = [
+        models.UniqueConstraint(
+            fields=["surname", "bio"],
+            condition=models.Q(desc="Жив"),
+            name = "unique_surname_bio"
+        ),
+    ]
+
+    def __str__(self):
+        return f"{self.surname}{self.name}"
+
+class Publisher(models.Model):
+    name= models.CharField('Название', unique=True, blank=True)
+
+class Book(models.Model):
+    title= models.CharField('Название', max_length=50)
+    id_publisher= models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    id_user= models.ManyToManyField(Users)
