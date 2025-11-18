6aeff5ad3ebead941ea23ea29b54f77580ce76bc
Маслакова Мария <masamaslakova@gmail.com>
2025-11-19 01:30:21 +0500

    ПОЛУЧИЛОСЬ

--- exile_app/urls.py
+++ exile_app/urls.py
@@ -2,5 +2,6 @@ from exile_app import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.online)
+    path('', views.online),
+    path('product/', views.products, name='products')
 ]
--- exile_app/views.py
+++ exile_app/views.py
@@ -3,3 +3,12 @@ from .models import *
 
 def online(request):
     return render(request, 'index.html')
+
+def products(request):
+    product_number = int(request.GET.get('number', 2))
+    
+    products_from_db = Products.objects.all()[product_number - 1]
+    
+    return render(request, 'db.html', {
+        'product': products_from_db  
+    })
